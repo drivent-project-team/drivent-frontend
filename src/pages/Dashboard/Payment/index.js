9c272e2766d11b67730c74df3e81c7eedea645c1
Brenda Rayane <brenda.rayane.0018@gmail.com>
2023-03-05 17:09:21 -0300

    feat: page change

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -6,14 +6,16 @@ import TicketTypeOptions from '../../../components/Dashboard/PaymentArea/TicketO
 import TicketContext from '../../../contexts/TicketContext';
 import HotelOptions from '../../../components/Dashboard/PaymentArea/TicketOptions/HotelOptions';
 import ReservationButton from '../../../components/Dashboard/PaymentArea/TicketOptions/ReservationButton';
+import Index from '../../../components/Dashboard/PaymentArea/Index';
 
 export default function Payment() {
   const [status, setStatus] = useState(null);
-  const [ticketTypes, setTicketTypes] = useState([]);
+  const [ticketTypes, setTicketTypes] = useState(null);
+  const [changePage, setChangePage] = useState('tickets');
 
   const token = useToken();
 
-  const { ticketTypeSelected, showHotelOptions, showReservationButton, reservationSummary } = useContext(TicketContext);
+  const { ticketTypeSelected, showHotelOptions, showReservationButton } = useContext(TicketContext);
 
   useEffect(() => {
     requistion();
@@ -43,22 +45,26 @@ export default function Payment() {
   if (status === null) {
     return 'Carregando';
   }
-  console.log(reservationSummary);
 
   return (
     <>
       <TitlePage>Ingresso e pagamento</TitlePage>
-      <ContainerPayment>
-        {status === 404 ? (
-          <span>
-            Você precisa completar sua inscrição antes <br /> de prosseguir pra escolha de ingresso
-          </span>
-        ) : (
-          <TicketTypeOptions tickets={ticketTypes} />
-        )}
-        {showHotelOptions ? <HotelOptions ticketTypes={ticketTypes} ticket={ticketTypeSelected} /> : null}
-        {showReservationButton || ticketTypeSelected.name === 'online' ? <ReservationButton /> : null}
-      </ContainerPayment>
+      {changePage === 'tickets' ? 
+        <ContainerPayment>
+          {status === 404 ? (
+            <span>
+              Você precisa completar sua inscrição antes <br /> de prosseguir pra escolha de ingresso
+            </span>
+          ) : ticketTypes !== null ? (
+            <TicketTypeOptions tickets={ticketTypes} />
+          ) : (
+            'carregando'
+          )}
+          {showHotelOptions ? <HotelOptions ticketTypes={ticketTypes} ticket={ticketTypeSelected} /> : null}
+          {showReservationButton || ticketTypeSelected.name === 'online' ? <ReservationButton setChangePage={setChangePage}/> : null}
+        </ContainerPayment>
+        :
+        <Index /> }
     </>
   );
 }
